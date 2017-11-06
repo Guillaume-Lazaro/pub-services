39cdb43fc610b4ae080b73194ad6bceec837af5a
Guillaume Lazaro <guillaume.lazaro@gmail.com>
2017-11-06 11:25:23 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var speak= require('./speak');
+var Math= require('./Math');
+const _ = require('lodash');
+
+// 2éme exercice:
+var a = 5;
+var b = 3;
+
+console.log(Math.add('b',b));
+console.log(Math.sub(a,b));
+console.log(Math.mult(a,b));
+console.log(Math.div(a,b));
+
+
